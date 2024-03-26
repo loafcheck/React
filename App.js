50eed8fd25f8ae6a14cae66a574ddd3c1a0b6517
loafcheck <rachelskim.contact@gmail.com>
2024-03-26 14:50:06 -0700

    delete function, addPostTitle function

--- App.js
+++ App.js
@@ -12,53 +12,110 @@ function App() {
    let [logo,setlogo] = useState('ReactBlog');
    let [likes, setlikes] = useState([0,0,0,0]);
    let [modal, setModal] = useState(false);
+   let [selectedIndex, setSelectedIndex] = useState(null);
+   let [input, setInput] = useState(null);
+
    function updatePostTitle () {
     let newPostTitle = [...postTitle];
     newPostTitle[0] = 'Pacific Catch';
     setPostTitle(newPostTitle);
    }
 
+   function handleAddTitles () {
+    if(input.trim !== ''){
+      const newPostTitles = [input, ...postTitle];
+      setPostTitle(newPostTitles);
+      setInput('');
+    }
+  }
+
+  function deletePost (index) {
+    const deletedPosts =[...postTitle].filter((_, indexParameter)=>{ return indexParameter != index});
+    setPostTitle(deletedPosts);
+
+  }
+
   return (
     <div className="App">
+      
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
+      
       <button onClick={()=>{
         const sortedPostTitles = [...postTitle].sort();
-        setPostTitle(sortedPostTitles);}}>A-Z Order</button>
+        setPostTitle(sortedPostTitles);}}>
+        A-Z Order
+      </button>
+      
       {
-        postTitle.map(function(a, i){
+        postTitle.map(function(title, index){
           return (
-            <div className='list'>
-            <h4 onClick={()=>{setModal(!modal)}}>
-            {postTitle[i]}
-            <span onClick={()=>{
+            <div className='list' key={index}>
+            <h4 onClick={()=>{
+              setModal(!modal) 
+              setSelectedIndex(index)
+              }} >
+    
+            {postTitle[index]}
+        
+            <span onClick={(e)=>{
+              e.stopPropagation();
               const newlikes = [...likes];
-              newlikes[i] += 1;
+              newlikes[index] += 1;
               setlikes(newlikes);
-              }}>👍</span> {likes[i]}
+              }}>
+              👍
+              </span>
+              {likes[index]}
             </h4>
+
+            <button onClick={()=>{deletePost(index)}}>delete</button>
+
             <p>Feb.18.2024</p>
             </div>
           )
         })
       }
+     
+      <span>win{likes[0]}</span>
+    <button 
+        onClick={()=>{
+        const newLikes = [...likes];
+        newLikes[0] += 1;
+        setlikes(newLikes);
+      }}>click</button>
 
       {
-        modal == true ? <Modal updatePostTitle={updatePostTitle} setPostTitle= {setPostTitle} postTitle={postTitle} color={'skyblue'}></Modal> : null
+        modal == true ? <Modal  selectedIndex={selectedIndex} updatePostTitle={updatePostTitle} setPostTitle= {setPostTitle} postTitle={postTitle} color={'skyblue'}></Modal> : null
       }
+
       
+      <input style={{display:'block', margin:'auto', marginTop:'30px'}}
+      onChange={(e)=>{setInput(e.target.value)}}></input>
+      <button 
+      style={{background: 'skyblue', borderRadius: '5px', marginTop: '10px'}}
+      onClick={handleAddTitles}>
+      enter</button>
+      
+
   </div>
+  
   );
 }
+
+
+
+
 
 function Modal (props) {
   return (
     <div className='modal' style={{background: props.color}}>
-      <h4>{props.postTitle[0]}</h4>
+      <h4>{props.postTitle[props.selectedIndex]}</h4>
       <p>Date</p>
       <p>Content</p>
-      <button onClick={()=>{props.updatePostTitle()}}>Edit</button>
+      <button onClick={()=>{props.updatePostTitle()}}>Edit Title</button>
+      <button onClick={()=>{props.setPostTitle(['pacific Catch', 'SweetMape', 'Verve', 'bluebottle'])}}>Edit Title2</button>
     </div>
   )
 }
